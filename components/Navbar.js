ee8c75e216a51d15119b90d8caf0b60109ac1ecb
DarshilGajera07 <darshilgajera07@gmail.com>
2024-08-31 11:34:33 +0530

    39 done cartpage design.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import React, { useRef } from 'react'
+import { AiFillCloseCircle, AiFillMinusCircle, AiFillPlusCircle, AiOutlineMinusCircle, AiOutlinePlusCircle, AiOutlineShoppingCart } from 'react-icons/ai'
+import { BsFillBagCheckFill } from 'react-icons/bs'
 
 const Navbar = () => {
+  const togglecart = () => {
+    if (ref.current.classList.contains('translate-x-full')) {
+      ref.current.classList.remove('translate-x-full')
+      ref.current.classList.add('translate-x-0')
+    }
+    else if (!ref.current.classList.contains('translate-x-full')) {
+      ref.current.classList.remove('translate-x-0')
+      ref.current.classList.add('translate-x-full')
+    }
+
+  }
+  const ref = useRef()
   return (
     <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md'>
-      <div className='logo mx-5'>  
+      <div className='logo mx-5'>
         <Link href={'/'}>
-        <Image src={"/logo.jpeg"} alt='' width={"100"} height={"100"}/>
+          <Image src={"/logo.jpeg"} alt='' width={"100"} height={"100"} />
         </Link>
       </div>
       <div className='nav'>
@@ -19,10 +32,45 @@ const Navbar = () => {
           <Link href={'/tshirts'}><li>Tshirts</li></Link>
         </ul>
       </div>
-      <div className='cart absolute right-0 top-4 mx-5'>
-        <AiOutlineShoppingCart className='text-xl md:text-2xl'/>
+      <div onClick={togglecart} className='cursor-pointer cart absolute right-0 top-4 mx-5'>
+        <AiOutlineShoppingCart className='text-xl md:text-2xl' />
       </div>
-      
+
+      <div ref={ref} className='sideCart w-72 h-full absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform translate-x-full'>
+        <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+        <span onClick={togglecart} className='absolute top-4 right-2 cursor-pointer text-2xl'><AiFillCloseCircle /></span>
+        <ol className='list-decimal font-semibold'>
+          <li>
+            <div className='item flex my-5'>
+              <div className='w-2/3 font-semibold'>Tshirts</div>
+              <div className='flex items-center justify-center w-1/3 font-semibold text-lg'>
+              <AiFillMinusCircle className='cursor-pointer'/><span className='mx-2'> 1 </span> <AiFillPlusCircle className='cursor-pointer'/>  
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className='item flex my-5'>
+              <div className='w-2/3 font-semibold'>Tshirts</div>
+              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
+            </div>
+          </li>
+          <li>
+            <div className='item flex my-5'>
+              <div className='w-2/3 font-semibold'>Tshirts</div>
+              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
+            </div>
+          </li>
+          <li>
+            <div className='item flex my-5'>
+              <div className='w-2/3 font-semibold'>Tshirts</div>
+              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
+            </div>
+          </li>
+        </ol>
+        
+    <button className="flex mt-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"><BsFillBagCheckFill className='m-1'/>Checkout</button>
+      </div>
+
     </div>
   )
 }
