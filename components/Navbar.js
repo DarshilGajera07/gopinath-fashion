01d222a22223d0e2fa8680fcf3431f8dcfe5a268
DarshilGajera07 <darshilgajera07@gmail.com>
2024-09-01 15:47:11 +0530

    42 done. cart state(plus, minus, clear,add)

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,8 @@ import React, { useRef } from 'react'
 import { AiFillCloseCircle, AiFillMinusCircle, AiFillPlusCircle, AiOutlineMinusCircle, AiOutlinePlusCircle, AiOutlineShoppingCart } from 'react-icons/ai'
 import { BsFillBagCheckFill } from 'react-icons/bs'
 
-const Navbar = () => {
+const Navbar = ({cart, addtocart, removefromcart, clearcart, subtotal}) => {
+  console.log(cart, addtocart, removefromcart, clearcart, subtotal)
   const togglecart = () => {
     if (ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-full')
@@ -40,37 +41,23 @@ const Navbar = () => {
         <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
         <span onClick={togglecart} className='absolute top-4 right-2 cursor-pointer text-2xl'><AiFillCloseCircle /></span>
         <ol className='list-decimal font-semibold'>
-          <li>
+          {Object.keys(cart).length == 0 && 
+          <div className='my-4 font-semibold'>Your cart is Empty!</div>
+          }
+         {Object.keys(cart).map((k)=>{return <li key={k}>
             <div className='item flex my-5'>
-              <div className='w-2/3 font-semibold'>Tshirts</div>
+              <div className='w-2/3 font-semibold'>{cart[k].name}</div>
               <div className='flex items-center justify-center w-1/3 font-semibold text-lg'>
-                <AiFillMinusCircle className='cursor-pointer' /><span className='mx-2'> 1 </span> <AiFillPlusCircle className='cursor-pointer' />
+                <AiFillMinusCircle onClick={()=>{removefromcart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].varient)}} className='cursor-pointer' /><span className='mx-2'> {cart[k].qty} </span> <AiFillPlusCircle onClick={()=>{addtocart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].varient)}} className='cursor-pointer' />
               </div>
             </div>
-          </li>
-          <li>
-            <div className='item flex my-5'>
-              <div className='w-2/3 font-semibold'>Tshirts</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
-            </div>
-          </li>
-          <li>
-            <div className='item flex my-5'>
-              <div className='w-2/3 font-semibold'>Tshirts</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
-            </div>
-          </li>
-          <li>
-            <div className='item flex my-5'>
-              <div className='w-2/3 font-semibold'>Tshirts</div>
-              <div className='flex items-center justify-center w-1/3 font-semibold'>1</div>
-            </div>
-          </li>
+          </li>})}
+      
         </ol>
 
         <div className='flex'>
           <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm"><BsFillBagCheckFill className='m-1' />Checkout</button>
-          <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">Clear Cart</button>
+          <button onClick={clearcart} className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">Clear Cart</button>
         </div>
       </div>
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,74 @@
+import { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const [cart, setcart] = useState({})
+  const [subtotal, setsubtotal] = useState(0)
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("cart")) {
+        setcart(JSON.parse(localStorage.getItem("cart")))
+      } 
+    } catch (error) {
+      console.error(error);
+      localStorage.clear()
+      
+    }
+ 
+  }, [])
+  
+
+  const savecart = (mycart) =>{
+    localStorage.setItem('cart', JSON.stringify(mycart))
+    let subt=0;
+    let keys = Object.keys(mycart)
+    for (let i = 0; i<keys.length; i++) {
+      subt += mycart[keys[i]].price * mycart[keys[i].qty]
+      
+    }
+    setsubtotal(subt)
+  }
+
+  const addtocart = (itemcode,qty, price, name, size, variant)=>{
+    let newcart = cart;
+    if (itemcode in cart) {
+      
+      newcart[itemcode].qty = cart[itemcode].qty + qty
+    }
+    else{
+      newcart[itemcode] = {qty:1, price, name, size, variant}
+    }
+    setcart(newcart)
+    savecart(newcart)
+
+  }
+
+  const clearcart = () =>{
+    setcart({})
+    savecart({})
+  }
+
+
+  const removefromcart = (itemcode,qty, price, name, size, variant)=>{
+    let newcart = cart;
+    if (itemcode in cart) {
+      
+      newcart[itemcode].qty = cart[itemcode].qty - qty
+    }
+    if (newcart[itemcode]["qty"]<=0) {
+      delete newcart[itemcode]
+    }
+    setcart(newcart)
+    savecart(newcart)
+
+  }
+
   return <>
-    <Navbar />
-    <Component {...pageProps} />
+    <Navbar cart={cart} addtocart={addtocart} removefromcart={removefromcart} clearcart={clearcart} subtotal={subtotal} />
+    <Component cart={cart} addtocart={addtocart} removefromcart={removefromcart} clearcart={clearcart} subtotal={subtotal} {...pageProps} />
     <Footer />
   </>
 }
